5533670269260fd5e2db5a94a0d5dc3676b08db8
Sam Gbafa <skgbafa@gmail.com>
2024-08-09 10:21:31 -0400

    update project (#3)

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const config = {
   contentPath: process.env.CONTENT_PATH || "./documents",
   outputPath: process.env.OUTPUT_PATH || "./public",
-  subdomain: process.env.SUBDOMAIN || "cwt-example",
+  subdomain: process.env.SUBDOMAIN || "aleph",
   cfSuffix: "-wiki",
 };
 
